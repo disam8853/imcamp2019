d93fac00189e9665ba2338bdaa860e39623f317b
Sam <wang.sam33@gmail.com>
2019-03-01 16:25:51 +0800

    update

---
+++ signup/script.js
@@ -0,0 +1,9 @@
+var stepper = document.querySelector('.stepper');
+var stepperInstace = new MStepper(stepper, {
+    // options
+    firstActive: 0 // this is the default
+})
+$(document).ready(function() {
+    $('select').formSelect();
+    $('.nottoomuch').characterCounter();
+});
