e748877ac91931bba19a07f9341a7c5594346847
eethan1 <call132call132@gmail.com>
2019-03-03 16:52:32 +0800

    add warning and repr filter

--- signup/script.js
+++ signup/script.js
@@ -9,9 +9,54 @@ $(document).ready(function() {
 });
 
 
-var cols = ["name", "gender", "birthday", "ID", "school","grade","team", "dietPrefer", "illness", "size", "phone",     "fb", "email", "emergency", "relationship", "emPhone", "intro", "motivation", "expection"];
-var chin = ["名字","性別","生日","身份證字號","學校","年級","類組","飲食偏好","特殊疾病","營服尺寸","聯絡電話","facebook","email","緊急聯絡人","關係","緊急聯絡人電話","自我介紹","動機","期望"]
-var filters = [];
+var cols = ["Checked","name", "gender", "birthday", "ID", "school","grade","team", "dietPrefer", "illness", "size", "phone",     "fb", "email", "emergency", "relationship", "emPhone", "intro", "motivation", "expection"];
+var chin = ["Checked","名字","性別","生日","身份證字號","學校","年級","類組","飲食偏好","特殊疾病","營服尺寸","聯絡電話","facebook","email","緊急聯絡人","關係","緊急聯絡人電話","自我介紹","動機","期望"]
+var skipp = [0,0,1,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0];
+var filters = [
+    /.*/,
+    /.*/, 
+    /[male, female]{1}/,
+     /^[0-9]{4}\/[0-9]{1,2}\/[0-9]{1,2}$/,
+     /^[A-Z][1,0][0-9]{8}$/,
+    /.*/,
+    /.*/,
+    /.*/,
+    /.*/,
+    /.*/,
+    /.*/,
+    /^[0-9,\-]{5,12}$/,
+    /.*www\.facebook\.com\/.*/,
+    /.+@.+/,
+    /.*/,
+    /.*/,
+    /^[0-9,\-]{5,12}$/,
+    /.*/,
+    /.*/,
+    /.*/,
+    /.*/,
+];
+var warnmsg = [
+    "",
+    "",
+    "請填入性別",
+    "生日請按照 xxxx/xx/xx",
+    "身份證字號格式錯誤, Ex:A123456789",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "聯絡電話格式錯誤, Ex: 0987-654-321",
+    "facebook格式錯誤, Ex:www.facebook.com/www.dIsAn",
+    "email 格式錯誤, Ex: dOwOb@ntu.im",
+    "",
+    "",
+    "聯絡電話格式錯誤, Ex: 0987-654-321",
+    "",
+    "",
+]
+// dirty code
 var checker = function(event) {
     var inputs = $('input');
     var labels = $('label');
@@ -21,14 +66,20 @@ var checker = function(event) {
         return;
     }
     for(var i=1;i<inputs.length;++i) {
-        if(inputs[i].value === "" || inputs[i].value === null || typeof(inputs[i]) === undefined) {
+        if(skipp[i]==0 && (inputs[i].value === "" || inputs[i].value === null || typeof(inputs[i]) === undefined)) {
             alert(chin[i]+"未填寫");
+            event.preventDefault();
+            break;
+        }
+        if(skipp[i]==0 && inputs[i].value.match(filters[i]) === null) {
+            alert(warnmsg[i]);
             event.preventDefault();
             break;
         }
     }    
     return;    
 }
+
 $('form').submit(checker);
 var tester = function() {
     var inputs = $('input');
